7abbd89c61c3e6e26ff7fbde8d2b10ba06a48341
Samuel Bósquez <samuelbosquez02@gmail.com>
2025-09-15 02:07:07 -0500

    Versión inicial de mi portafolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./mywork.html",
+    "./contact.html",
+    "./dist/output.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
